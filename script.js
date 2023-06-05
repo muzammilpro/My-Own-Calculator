be7f6e8655a757047df4450ce550230092af9dbe
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-06-05 22:11:17 +0500

    JS Added

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-var inputValue = parseInt(document.getElementById("inputText").value)
+var result = document.getElementById("result");
+
+function appendToResult(value) {
+  result.value += value;
+}
+
+function clearResult() {
+  result.value = "";
+}
+
+function calculate() {
+  try {
+    var calculation = eval(result.value);
+    result.value = calculation;
+  } catch (error) {
+    result.value = "Error";
+  }
+}
