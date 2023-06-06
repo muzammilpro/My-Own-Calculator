ecef3b5be18405822123e88ba6636a664da50fe5
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-06-06 12:42:13 +0500

    Calculator Completed

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-var result = document.getElementById("result");
 
-function appendToResult(value) {
-  result.value += value;
-}
 
-function clearResult() {
-  result.value = "";
-}
 
-function calculate() {
-  try {
-    var calculation = eval(result.value);
-    result.value = calculation;
-  } catch (error) {
-    result.value = "Error";
-  }
-}
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the input element
+  const inputText = document.getElementById('inputText');
+
+  // Get all the calculator buttons
+  const buttons = document.querySelectorAll('.buttons button');
+
+  // Attach click event listener to each button
+  buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      const buttonText = this.innerText;
+
+      if (buttonText === '=') {
+        // Evaluate the expression and display the result
+        try {
+          const result = eval(inputText.value);
+          inputText.value = result;
+        } catch (error) {
+          inputText.value = 'Error';
+        }
+      } else if (buttonText === 'C') {
+        // Clear the input field
+        inputText.value = '';
+      } else {
+        // Append the clicked button's value to the input field
+        inputText.value += buttonText;
+      }
+    });
+  });
+});
